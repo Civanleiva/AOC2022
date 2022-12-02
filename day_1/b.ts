41e4295ebd0674d606288cd7c67c0f9e609d8324
Carlos <civanleiva42@gmail.com>
2022-12-02 12:47:36 +0100

    Day 1

---
+++ day_1/b.ts
@@ -0,0 +1,15 @@
+export const x = '';
+
+const input = await Deno.readTextFile('./input.txt');
+
+const elves = input
+  .split('\r\n\r\n')
+  .map((elve: string) => elve.replace(/\n/g, '').replace(/\r/g, '+'));
+
+const totalCalories = elves.map((elve) => eval(elve));
+totalCalories.sort((a, b) => a - b);
+
+const topThreeElves =
+  totalCalories.pop() + totalCalories.pop() + totalCalories.pop();
+
+console.log(topThreeElves);
