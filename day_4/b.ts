59358d2ff1d9c0da552f26b3d3d376947232881b
Carlos <civanleiva42@gmail.com>
2022-12-05 15:39:02 +0100

    Day 4

---
+++ day_4/b.ts
@@ -0,0 +1,46 @@
+const input = await Deno.readTextFile('./input.txt');
+const fixedInput = input.replace(/\r/g, '').split('\n');
+const assignments: number[][][] = [];
+let total = 0;
+
+const elfPairs = fixedInput.map((pair) => {
+  return pair.split(',');
+});
+
+const populateArray = (start: number, end: number) => {
+  return Array.from(
+    Array.from(Array(Math.ceil(end - start + 1)).keys()),
+    (x) => start + x
+  );
+};
+
+const _findDomain = (elf: string) => {
+  const elfSections = elf.split('-');
+  return elfSections;
+};
+
+elfPairs.forEach((elfPair) => {
+  const tasks: number[][] = [];
+  elfPair.forEach((elf) => {
+    const elfSections = elf.split('-').map((e) => parseInt(e));
+    const assignments = populateArray(elfSections[0], elfSections[1]);
+    tasks.push(assignments);
+  });
+  assignments.push(tasks);
+});
+
+assignments.forEach((assignment) => {
+  const firstOverlaps = assignment[0].some((element) => {
+    return assignment[1].includes(element);
+  });
+
+  const secondOverlaps = assignment[1].some((element) => {
+    return assignment[0].includes(element);
+  });
+
+  if (firstOverlaps || secondOverlaps) {
+    total += 1;
+  }
+});
+
+console.log(total);
